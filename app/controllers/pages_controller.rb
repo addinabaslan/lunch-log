9e1a503b93831629476a3a866c0d227b7debd073
addinabaslan <addina@rambutan.tech>
2020-03-13 11:11:47 +0800

    Fix sign up features

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,10 @@ class PagesController < ApplicationController
 
     def index
     end
+
     def signup
     end
+    
     def login
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_213537) do
+ActiveRecord::Schema.define(version: 2020_03_11_041219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
