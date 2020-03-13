7e23443510d20e046342fc09de61fb8906d263cd
addinabaslan <addina@rambutan.tech>
2020-03-13 11:10:53 +0800

    Fix routes for user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root 'posts#index'
   get  '/login',   to: 'users#login'
-  get  '/register',  to: 'users#new'
-  post '/signup',  to: 'users#create'
+  get  '/register',  to: 'pages#signup'
+  post '/register',  to: 'users#create'
   post   '/login',   to: 'sessions#create'
   resources :users
-    
+  
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
